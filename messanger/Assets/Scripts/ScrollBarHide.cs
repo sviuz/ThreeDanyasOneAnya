67df04826cc745fed545f616a28d865efb368ac6
sviuz <60036237+sviuz@users.noreply.github.com>
2021-03-07 12:02:52 +0200

    Added hide scroll bar script. renamed some rus files to eng lang.

---
+++ messanger/Assets/Scripts/ScrollBarHide.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
+
+public class ScrollBarHide : MonoBehaviour
+{
+    [FormerlySerializedAs("SCrollLine")] public GameObject ScrollLine;
+    public ScrollRect scroll;
+
+    private void LateUpdate()
+    {
+        ScrollLine.SetActive(Mathf.Abs(scroll.velocity.y)>10&&scroll.content.sizeDelta.y>scroll.viewport.sizeDelta.y);
+    }
+}
