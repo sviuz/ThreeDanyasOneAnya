1f8ced293f766ba5801365d848cf7b78386ec1af
AnnBazille <79001425+AnnBazille@users.noreply.github.com>
2021-04-02 00:09:21 +0300

    Add files via upload

---
+++ DatabaseSetup/Database/MessengerDBContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace DatabaseSetup.Database
+{
+    public class MessengerDBContext : DbContext
+    {
+        public MessengerDBContext() : base("defaultConnection") { }
+        public DbSet<Chat> Chats { get; set; }
+        public DbSet<EventType> EventTypes { get; set; }
+        public DbSet<Format> Formats { get; set; }
+        public DbSet<Log> Logs { get; set; }
+        public DbSet<Media> Medias { get; set; }
+        public DbSet<Message> Messages { get; set; }
+        public DbSet<Report> Reports { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<Type> Types { get; set; }
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ DatabaseSetup/Database/Type.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseSetup.Database
+{
+    public class Type
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
