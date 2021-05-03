6e995307fead130be8bfa87ac62b14704682fb16
sviuz <60036237+sviuz@users.noreply.github.com>
2021-05-03 22:45:47 +0300

    Added general Scene

---
+++ messanger/Assets/LoginButton.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LoginPanel : MonoBehaviour
+{
+    [SerializeField] private Text email;
+    [SerializeField] private Text password;
+    [SerializeField] private Button LoginButton;
+    void Start()
+    {
+        LoginButton.onClick.AddListener(OnLoginEnter);
+    }
+
+    private void OnLoginEnter(){
+        PlayerPrefs.SetString("user_email", email.text);
+        PlayerPrefs.SetString("user_password", password.text);
+        //SceneManager.LoadScene("Menu");
+    }
+}
---
+++ messanger/Assets/Scripts/NewUIScripts/LogRegWindow.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LogRegWindow : MonoBehaviour
+{
+    [SerializeField]private Button Login;
+    [SerializeField]private Button Register;
+    
+    void Start()
+    {
+            
+    }
+}
